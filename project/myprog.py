abaefad86b362399c3b23a5ed517299492e6dded
Reuven Lerner <reuven@lerner.co.il>
2021-10-11 20:10:06 +0300

    gitautosync update 12

--- project/myprog.py
+++ project/myprog.py
@@ -1,4 +1,12 @@
+# comments are for the developers/maintainers
+# API documentation is for the people who will call/use the function
+#  we write API documentation in Python in "docstrings"
+#  meaning: if a string (text) is the first line of a function, that the doc
+
 def hello(name):
+    """This is the most amazing function ever written!
+
+    So great, it gets a two-line docstring!"""
     return f'Hello, {name}!'
 
 
